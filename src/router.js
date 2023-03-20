e68699ed972ef40ed880bc55307e027c1b56f2d1
@s.roertgen <sroertgen@gmail.com>
2023-03-20 09:40:20 +0100

    remove endpoints
    Reconciliation has to happen against a specific dataset.
    Therefore generic endpoints can be deleted.

--- src/router.js
+++ src/router.js
@@ -11,23 +11,16 @@ const routes = express.Router()
 
 // 1. GET: either return service manifest or do reconciliation query (if ?queries parameter given)
 routes.route('/_reconcile/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})/:dataset([a-zA-Z0-9](([a-zA-Z0-9%.:_-]|\/[^_]){0,}))').get(controller.dataset)
-routes.route('/_reconcile/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})').get(controller.dataset)
-routes.route('/_reconcile').get(controller.dataset)
 
 // 2. POST: do a reconciliation query
 routes.route('/_reconcile/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})/:dataset([a-zA-Z0-9](([a-zA-Z0-9%.:_-]|\/[^_]){0,}))').post(controller.query)
-routes.route('/_reconcile/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})').post(controller.query)
-routes.route('/_reconcile').post(controller.query)
 
 // 3. GET: do a preview
 routes.route('/_preview/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})/:dataset([a-zA-Z0-9](([a-zA-Z0-9%.:_-]|\/[^_]){0,}))(/:id([a-zA-Z0-9][a-zA-Z0-9%.:/_-]{0,}))?').get(controller.preview)
-routes.route('/_preview/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})').get(controller.preview)
-routes.route('/_preview').get(controller.preview)
 
 // 4. GET: give a suggestion
 routes.route('/_suggest/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})/:dataset([a-zA-Z0-9](([a-zA-Z0-9%.:_-]|\/[^_]){0,}))').get(controller.suggest)
-routes.route('/_suggest/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})').get(controller.suggest)
-// routes.route('/_suggest/entity').get(controller.suggest)		// query parameters are: "prefix" and "cursor"
+
 
 // 5. other services, not implemented yet
 routes.route('/_extend/:account([a-zA-Z0-9][a-zA-Z0-9_-]{0,})/:dataset([a-zA-Z0-9](([a-zA-Z0-9%.:_-]|\/[^_]){0,}))').post(controller.extend)
