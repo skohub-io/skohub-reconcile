10117ba5e0b6f6561d0b8d0b9c0d98ab467868aa
@s.roertgen <sroertgen@gmail.com>
2023-04-04 15:11:31 +0200

    add definition, id and scope note to flyout

--- src/controller/flyout.js
+++ src/controller/flyout.js
@@ -2,14 +2,23 @@ import { getParameters } from "./utils.js";
 import queryID from "../esQueries/queryID.js";
 import config from "../config.js";
 
-const defaultLanguage = config.app_defaultlang ? config.app_defaultlang : 'en'
+const defaultLanguage = config.app_defaultlang ? config.app_defaultlang : "en";
 
 const buildHtml = (result, language) => {
-  const prefLabel = result?.prefLabel[language || defaultLanguage] || "ID not found";
-  const html = `<p style=\"font-size: 0.8em; color: black;\">${prefLabel}</p>`;
+  const prefLabel =
+    result?.prefLabel[language || defaultLanguage] || "ID not found";
+  const definition = result?.definition?.[language || defaultLanguage] || ""
+  const id = result?.id || "";
+  const scopeNote = result?.scopeNote?.[language || defaultLanguage] || "";
+
+  const html = `<div>
+                  <h1>${prefLabel}</h1>
+                  <p style=\"font-size: 0.8em; color: black;\">ID: ${id}</p>
+                  <p style=\"font-size: 0.8em; color: black;\">Definition: ${definition}</p>
+                  <p style=\"font-size: 0.8em; color: black;\">Scope Note: ${scopeNote}</p>
+                </div>`;
   return html;
 };
-
 
 export default async function flyout(req, res) {
   const { account, dataset, language } = getParameters(req);
