e82c2b79babee783a09b8a75b945e85724416324
@s.roertgen <sroertgen@gmail.com>
2023-04-12 17:04:55 +0200

    better mock query

--- src/controller/manifest/index.test.js
+++ src/controller/manifest/index.test.js
@@ -1,7 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { queryResponse, validManifest } from "../__mocks__/manifest.js";
 import manifest from "./index.js";
-
+import * as esQueries from "../../queries/index.js";
 
 
 vi.mock("../utils.js" , async () => {
@@ -12,6 +12,14 @@ vi.mock("../utils.js" , async () => {
   }
 })
 
+vi.mock("../../queries/index.js" , async () => {
+  return {
+    default: {
+      query: null
+    }
+  }
+})
+
 
 describe("Manifest", () => {
   afterEach(() => {
@@ -19,13 +27,7 @@ describe("Manifest", () => {
   })
 
   it("correctly passes requests", async () => {
-    vi.mock("../../queries/index.js" , async () => {
-      return {
-        default: {
-          query: vi.fn().mockResolvedValue(queryResponse),
-        }
-      }
-    })
+    esQueries.default.query = vi.fn().mockResolvedValue(queryResponse);
     const req = {
       query: {
         account: "dini-ag-kim",
@@ -41,6 +43,22 @@ describe("Manifest", () => {
     expect(res.send).toBeCalledWith(validManifest);
   });
 
+  it("not passing request", async () => {
+    esQueries.default.query = vi.fn().mockResolvedValue([]);
+    const req = {
+      query: {
+        account: "dini-ag-kim",
+        dataset: "https://w3id.org/rhonda/polmat/scheme",
+        language: "en",
+      },
+      params: {},
+    };
+    const res = {
+      send: vi.fn(),
+    };
+    await manifest(req, res);
+    expect(res.send).toBeCalledWith(validManifest);
+  });
 });
 
   it.todo("should return no manifest since dataset or account is not present")
