d7c59bc33390663fa6df3a368700e6a62ac740bd
@s.roertgen <sroertgen@gmail.com>
2023-04-14 13:17:28 +0200

    lowercase prefix

--- src/queries/suggest.js
+++ src/queries/suggest.js
@@ -42,7 +42,7 @@ export default async function suggest(
               {
                 wildcard: {
                   [`${n}.${language}`]: {
-                    value: `*${prefix}*`,
+                    value: `*${prefix.toLowerCase()}*`,
                     boost: 1.0,
                   },
                 },
