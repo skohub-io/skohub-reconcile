fcbbb6a51ef8c39bd6b77a11d7660b3b616f3a13
@s.roertgen <sroertgen@gmail.com>
2023-08-24 16:55:30 +0200

    adjust routes according to spec (delete underscores)

--- src/router.js
+++ src/router.js
@@ -5,18 +5,18 @@ const routes = express.Router()
 routes.route('/ping').get(controller.ping)
 
 // GET: either return service manifest or do reconciliation query (if ?queries parameter given)
-routes.route('/_reconcile').get(controller.reconcile)
+routes.route('/reconcile').get(controller.reconcile)
 
 // POST: do a reconciliation query
-routes.route('/_reconcile').post(controller.query)
+routes.route('/reconcile').post(controller.query)
 
 // GET: do a preview
-routes.route('/_preview').get(controller.preview)
+routes.route('/preview').get(controller.preview)
 
 // GET: give a suggestion
-routes.route('/_suggest').get(controller.suggest)
+routes.route('/suggest').get(controller.suggest)
 
 // GET: flyout
-routes.route('/_suggest/_flyout').get(controller.flyout)
+routes.route('/suggest/flyout').get(controller.flyout)
 
 export { routes }
