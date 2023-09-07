b0206ec37752e95f2cf98c3906f84ab487f8c589
Steffen Rörtgen <sroertgen@gmail.com>
2023-09-07 11:03:02 +0200

    23 add patch number (#27)
    * add patch number to prerelease suffix (According to https://semver.org/#spec-item-9 a patch number has to be included when using prerelease suffix.)
    * validate with semver

--- src/controller/manifest/buildManifest.test.js
+++ src/controller/manifest/buildManifest.test.js
@@ -1,8 +1,8 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { queryResponse, validManifest } from "./__mocks__/manifest.js";
+import { valid } from 'semver';
 
 const qRes = queryResponse
-
 const { buildManifest } = await import("./buildManifest.js");
 
 describe("Build manifest", () => {
@@ -10,6 +10,6 @@ describe("Build manifest", () => {
     const manifestRes = await buildManifest(qRes, "dini-ag-kim", "https://w3id.org/rhonda/polmat/scheme", "en");
     expect(manifestRes).toStrictEqual(validManifest)
     // TODO validate manifest against schema
-
+    expect(manifestRes.versions.every(v => valid(v))).toBeTruthy()
   });
 });
