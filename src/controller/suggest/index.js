2746339cee5b3523a3bb813edc6eb97ab63d9a76
@s.roertgen <sroertgen@gmail.com>
2023-09-18 15:48:36 +0200

    shorten line

--- src/controller/suggest/index.js
+++ src/controller/suggest/index.js
@@ -11,9 +11,7 @@ export default async function suggest(req, res, next) {
 
   try {
     await checkAccountDataset(res, account, dataset);
-    if (!prefix) {
-      throw new ReconcileError("No prefix provided", 400);
-    }
+    if (!prefix) throw new ReconcileError("No prefix provided", 400);
     const result = await queryForSuggestions(account, dataset, prefix, cursor, language);
     return res.json({
       result: result,
